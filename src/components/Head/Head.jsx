0b051477f2a4c4854f6d34befdff21544ad08869
Harish-b-03 <harishbalaji369@gmail.com>
2022-09-25 14:26:06 +0530

    Filter Function Added

--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -1,8 +1,56 @@
 import "./Head.css"
 import property from "../../data"
 import Card from "../Card/Card"
+import { useState } from "react"
 
 function Head() {
+  const [list, setlist] = useState(property)
+  const changeStyle = () => {
+    const element = document.getElementById("PropertyLists");
+    if(list.length < 3){       
+        element.style.justifyContent = "left";
+        element.style.marginRight = "20px";
+    } else{
+        element.style.justifyContent = "space-between";
+        element.style.marginRight = "0";
+    }
+  }
+  const get_values = () => {
+    var location = document.getElementById("Location").value;
+    var when = document.getElementById("When").value;
+    var price = document.getElementById("Price").value;
+    var propertyType = document.getElementById("PropertyType").value;
+    if(when === "select"){
+        alert("Please Select Move-in Date")
+    }
+    else{
+        if((location !== undefined) && (when !== undefined) && (price !== undefined) && (propertyType !== undefined)){
+            return {data:{location, when, price, propertyType}, error:false}
+        }
+    }
+
+    return {data:{}, error: true}
+
+  }
+  const filter = () => {
+    const PropertyLists = document.getElementById("PropertyLists");
+    const {data, error} = get_values();
+    if(!error){
+        setlist(property.filter((element) => {
+            if((data.location === element.state)){
+                if(((data.price === "500-2500") && (element.rent >= 500 || element.rent <= 2500)) || ((data.price === "2500") && (element.rent > 2500))){
+                    if((data.propertyType === element.house_type)){
+                        if( ((data.when === "ready") && (element.move_in <= 15)) || ((data.when === "month") && (element.move_in <= 30 && element.move_in > 15)) || ((data.when === "more") && (element.move_in > 30))){
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }))
+        console.log(list,property,data)
+    }
+  }
   return (
     <div className="HeadContainer">
         <div className="layer1">
@@ -17,14 +65,14 @@ function Head() {
         <div className="FilterSectionContainer">
             <div className="LocationContainer">
                 <span className="Label">Location</span>
-                <select name="Location" className="Location">
-                    <option value="ny">New York, USA</option>
-                    <option value="fl">Florida, USA</option>
+                <select id="Location" className="Location">
+                    <option value="NY">New York, USA</option>
+                    <option value="FL">Florida, USA</option>
                 </select>
             </div>
             <div className="WhenContainer">
                 <span className="Label">When</span>
-                <select name="When" className="When">
+                <select id="When" className="When">
                     <option value="select">Select Move-in Date</option>
                     <option value="ready">Ready to occupy</option>
                     <option value="month">Within a month</option>
@@ -33,28 +81,28 @@ function Head() {
             </div>
             <div className="PriceContainer">
                 <span className="Label">Price</span>
-                <select className="Price">
+                <select id="Price" className="Price">
                     <option value="500-2500">&#36;500 - &#36;2,500</option>
-                    <option value="2500-5000">&#36;2,500 - &#36;5,000</option>
-                    <option value="5000-10000">&#36;5,000 - &#36;10,000</option>
-                    <option value="10000">Above &#36;10,000</option>
+                    <option value="2500">Above &#36;2,500</option>
                 </select>
             </div>
             <div className="PropertyTypeContainer">
                 <span className="Label">Property Type</span>
-                <select className="PropertyType">
-                    <option value="Houses">Houses</option>
+                <select id="PropertyType" className="PropertyType">
+                    <option value="House">Houses</option>
                     <option value="Villa">Villas</option>
                 </select>
             </div>
             <div className="SearchButtonContainer">
-                <button className="SearchButton">Search</button>
+                <button className="SearchButton" onClick={()=>{filter()}}>Search</button>
             </div>
         </div>
 
-        <div className="PropertyLists">
-            {property.map((data)=><Card key={data.id} data={data}/>)}
-        </div>
+        {
+        (list.length>0)?
+        <div id="PropertyLists" className={(list.length>=3)?"PropertyLists":"PropertyLists_short"}>
+            {list.map((data)=><Card key={data.id} data={data}/>)}
+        </div>:<div style={{marginTop:"50px",fontWeight:"700",}}>No matches found</div>}
     </div>
   )
 }
